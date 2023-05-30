c7725916d29acd67eac45a33ae9f68727a19e417
Steven-267 <aa030726@126.com>
2023-05-30 22:22:50 +0800

    -58

---
+++ pzmaven/un/src/test/java/com/Steven/DemoTest.java
@@ -0,0 +1,14 @@
+package com.Steven;
+
+public class DemoTest {
+    public  void test1(){
+        System.out.println("hi test1");
+    }
+
+    public  void test2(){
+        System.out.println("hi test2");
+    }
+    public  void test3(){
+        System.out.println("hi test3");
+    }
+}
